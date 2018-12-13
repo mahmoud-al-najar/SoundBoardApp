d30743fa232eaeb8fc4b0ae46271b0d5b7580f53
msryzhov <msryzhov@gmail.com>
2018-12-13 10:20:19 +0100

    Added GridPanel cmp

---
+++ components/GridPanel.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class GridPanel extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render(){
+        return (
+          <View style={styles.gridContainer}>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+            <View style={styles.box}>
+            </View>
+          </View>
+		);
+    }
+}
+
+const styles = StyleSheet.create({
+    gridContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+
+  box : {
+    backgroundColor : 'red',
+    height: 80,
+    margin: 10,
+    width: 80
+  }
+});
+
+export default GridPanel;
