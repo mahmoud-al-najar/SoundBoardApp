6c2f6478c78c0821214c2195e9aa7a5a5d18cec7
mahmoud-al-najar <mahmoud.najar.95@gmail.com>
2018-12-13 10:43:31 +0100

    Modified CardListView

--- components/CardListView.js
+++ components/CardListView.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import {Container, Content, Card, CardItem, Left, Right, Body, Thumbnail, Spinner, Icon} from 'native-base';
-import {StyleSheet, Text, View, SectionList, FlatList} from 'react-native';
+import { Container, Content, Card, CardItem, Left, Right, Body, Thumbnail, Spinner, Icon } from 'native-base';
+import { StyleSheet, Text, View, SectionList, FlatList } from 'react-native';
 import SingleCardView from 'react-native-simple-card';
 import * as Progress from 'react-native-progress';
 
+// new
+import GridView, { SuperGridSectionList } from 'react-native-super-grid';
 
 class CardListView extends React.Component {
     constructor(props) {
@@ -14,54 +16,39 @@ class CardListView extends React.Component {
         }
     }
 
-    renderCard = (item) => {
-        return (
-            <Card
-                elevation={1}
-                shadowColor="rgb(50,50,50)"
-                shadowOpacity={1}
-                marginTop={100}
-                height={150}
-                width={150}>
-                <View style={styles.item}>
-
-                    <Text style={{padding: 10, fontSize: 18}}>
-                        {item.f}
-                    </Text>
-
-                    <Progress.Bar progress={0.3} width={125}/>
-                </View>
-            </Card>
-        );
-    }
-
     render() {
         return (
-            /* <View style={{flex:1, flexDirection: 'row', justifyContent:'space-around' }}>
-                 <FlatList
-                     data={this.state.data}
-                     renderItem={(item) => this.renderCard(item)}
-                     horizontal={false}
-                     noOfColumns={1}
-                 />
-             </View>
-                 )*/
+            <SuperGridSectionList
+                itemDimension={130}
+                sections={[
+                    { title: 'Category 1', data: this.state.data },
+                    { title: 'Category 2', data: this.state.data },
+                    { title: 'Category 3', data: this.state.data },
 
-            <View style={styles.container}>
-                <SectionList
-                    // horizontal={false}
-                    //  noOfColumns={2}
-                    sections={[
-                        {title: 'Category 1', data: this.state.data},
-                        {title: 'Category 2', data: this.state.data},
-                        {title: 'Category 3', data: this.state.data},
+                ]}
 
-                    ]}
-                    renderItem={({item}) => this.renderCard(item)}
-                    renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-                    // keyExtractor={(item, index) => index}
-                />
-            </View>
+                style={styles.gridView}
+                renderItem={({ item }) => (
+                    <Card
+                        elevation={1}
+                        shadowColor="rgb(50,50,50)"
+                        shadowOpacity={1}
+                        marginTop={100}
+                        height={150}
+                        width={150}>
+                        <View style={styles.item}>
+
+                            <Text style={{ padding: 10, fontSize: 18 }}>
+                                {item.f}
+                            </Text>
+
+                            <Progress.Bar progress={0.3} width={125} />
+                        </View>
+                    </Card>
+                )}
+                renderSectionHeader={({ section }) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+            />
+
         )
     }
 
@@ -90,9 +77,29 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         height: 150,
-        padding: 10
-
-    }
+        padding: 10,
+        margin: 10
+    },
+    gridView: {
+        paddingTop: 25,
+        flex: 1,
+    },
+    itemContainer: {
+        justifyContent: 'flex-end',
+        borderRadius: 5,
+        padding: 10,
+        height: 150,
+    },
+    itemName: {
+        fontSize: 16,
+        color: '#fff',
+        fontWeight: '600',
+    },
+    itemCode: {
+        fontWeight: '600',
+        fontSize: 12,
+        color: '#fff',
+    },
 });
 
 export default CardListView;
