1e174283ae23d7e576cf6a8fb8a4c51ce73049ec
mahmoud-al-najar <mahmoud.najar.95@gmail.com>
2018-12-13 14:59:51 +0100

    Modified sound list + durations

--- App.js
+++ App.js
@@ -4,32 +4,17 @@ import {List, ListItem} from "react-native-elements";
 import SearchListView from "./components/SearchListView"
 import CardListView from "./components/CardListView";
 import GridPanel from "./components/GridPanel";
+import DataManager from './DataManager';
 
 export default class App extends React.Component {
 
     constructor(props) {
         super(props);
 
-        let mydata = [
-            {
-                "f": "fname1",
-                "l": "lname1",
-                image: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-                key: '1'
-            },
-            {"f": "fname2", "l": "lname2", image: require('./assets/crime.png'), key: '2'},
-            {"f": "fname1", "l": "lname1", image: require('./assets/crime.png'), key: '3'},
-            {"f": "fname2", "l": "lname2", image: 'assets/crime.png', key: '4'},
-            {"f": "fname1", "l": "lname1", image: 'assets/crime.png', key: '5'},
-            {"f": "fname2", "l": "lname2", image: 'assets/crime.png', key: '6'},
-            {"f": "fname1", "l": "lname1", image: 'assets/crime.png', key: '7'},
-            {"f": "fname2", "l": "lname2", image: 'assets/crime.png', key: '8'},
-            {"f": "fname1", "l": "lname1", image: 'assets/crime.png', key: '9'},
-            {"f": "fname2", "l": "lname2", image: 'assets/crime.png', key: '10'},
-            {"f": "fname1", "l": "lname1", image: 'assets/crime.png', key: '11'}
-        ];
+        let dataManager = new DataManager();
+        
         this.state = {
-            data: mydata
+            data: dataManager.soundsArray
         };
     }
 
@@ -54,12 +39,3 @@ export default class App extends React.Component {
     }
 
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
--- DataManager.js
+++ DataManager.js
@@ -10,6 +10,7 @@ class DataManager {
         this.soundsArray = this.db['sounds'];
         this.soundsArray.forEach(s => { 
             s.audio = new AudioFile(s.filename, s.url);
+            s.key = s.filename + s.url;
           });
         this.categoriesArray = this.db['categories'];
     }
@@ -20,12 +21,19 @@ class AudioFile {
         this.filename = filename;
         this.audio = new Audio.Sound();
         this.playing = false;
+        this.duration = 0;
+        this.position = 0;
         this.playAudio = async () => {
             if (!this.playing){
                 try {
                     await this.audio.unloadAsync()
                     await this.audio.loadAsync({uri: path});
                     await this.audio.playAsync();
+                    this.audio.setOnPlaybackStatusUpdate((status)=>{
+                        this.duration = status.durationMillis;
+                        this.position = status.positionMillis;
+                        console.log('Duration: '+ status.durationMillis + '   --  Position: ' + status.positionMillis);
+                    })
                     this.playing = true;
                     this.audio.setVolumeAsync(1.0);
                 } catch (err) {
@@ -37,13 +45,29 @@ class AudioFile {
                     v = v - 0.1;
                     if (v > 0){
                         this.audio.setVolumeAsync(v);
+                        
+                        
                     } else {
+                        this.audio.setPositionAsync(this.duration);
                         clearInterval(fadeout);
                         this.playing = false;
+                        this.audio.unloadAsync();
                     }
                 }, 200);
             }
         }
+
+
+        // this._getPlaybackTimestamp() = () => {
+        //     if (
+        //       this.audio != null &&
+        //       this.state.soundPosition != null &&
+        //       this.state.soundDuration != null
+        //     ) {
+        //       return `${this._getMMSSFromMillis(this.state.soundPosition)} / ${this._getMMSSFromMillis(this.state.soundDuration)}`;
+        //     }
+        //     return '';
+        //   }
     }
 }
 
