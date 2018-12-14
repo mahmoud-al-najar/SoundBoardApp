a73b15d1963f1ccc5182b2d6e3c4804c3ecf343f
mahmoud-al-najar <mahmoud.najar.95@gmail.com>
2018-12-14 09:53:27 +0100

    Fixing favorite click (star) + fixing sound titles and tags

--- components/SearchListView.js
+++ components/SearchListView.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import {StyleSheet, Text, View, FlatList} from 'react-native';
-import {List, ListItem, SearchBar, Icon} from "react-native-elements";
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { List, ListItem, SearchBar, Icon } from "react-native-elements";
 
 class SearchListView extends React.Component {
     constructor(props) {
         super(props);
 
-        console.log(props.favorites.length);
-        
         props.data.forEach(item => {
             // item.isFavorite = false;
             if (props.favorites.find(s => s.url === item.url) != undefined) item.isFavorite = true;
             else item.isFavorite = false;
-            }
+        }
         );
+
+        let count = this.getCountFavorites(props.data);
+        // console.log('Count favs: ' + count);
 
         this.state = {
             data: props.data,
             loading: false,
             error: null,
+            countFavorites: count,
         };
         this.arrayholder = props.data;
+    }
+
+    getCountFavorites(list) {
+        let count = 0;
+        list.forEach(i => {
+            if (i.isFavorite) count++;
+        })
+        // console.log('inside: ' + count);
+        
+        return count;
     }
 
     searchFilterFunction = text => {
@@ -30,7 +42,7 @@ class SearchListView extends React.Component {
 
             return itemData.indexOf(textData) > -1;
         });
-        this.setState({data: newData});
+        this.setState({ data: newData });
     };
 
     renderHeader = () => {
@@ -47,33 +59,35 @@ class SearchListView extends React.Component {
     render() {
         return (
             <FlatList
+                key={this.state.countFavorites}
                 data={this.state.data}
-                renderItem={({item}) => (
+                renderItem={({ item }) => (
 
                     <ListItem
                         keyExtractor={item => item.key}
                         title={item.name}
                         //subtitle={item.tagsText}
                         roundAvatar
-                        leftIcon={{name: 'library-music'}}
+                        leftIcon={{ name: 'library-music' }}
                         rightIcon={<Icon
                             // raised
                             name='star'
-                            size={30}
-                            color={(item.isFavorite) ? '#F9AA25': '#9E9E9E'}
+                            size={40}
+                            color={(item.isFavorite) ? '#F9AA25' : '#9E9E9E'}
                             //type='font-awesome' 
                             onPress={() => {
                                 let temp = this.state.data;
                                 temp.forEach(i => {
-                                    if (i.key == item.key){
-                                        
+                                    if (i.key == item.key) {
+
                                         if (i.isFavorite) i.isFavorite = false;
                                         else i.isFavorite = true;
-                                        this.setState({data: temp});
-                                        this.forceUpdate();         
+                                        this.setState({ data: temp, countFavorites: this.getCountFavorites(temp) });                                        
+                                        // this.forceUpdate();
+                                        // console.log(this.getCountFavorites(temp));
                                     }
                                 });
-                            }}/>}
+                            }} />}
 
                         onPress={() => item.audio.playAudio()}
                     />
